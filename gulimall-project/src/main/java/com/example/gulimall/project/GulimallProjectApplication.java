0136b39546a25d2bffba81455355bb6ee6a730a9
Michel <1097853056@qq.com>
2020-04-06 18:10:20 +0800

    项目初始结构创建

---
+++ gulimall-project/src/main/java/com/example/gulimall/project/GulimallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gulimall.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProjectApplication.class, args);
+    }
+
+}
---
+++ gulimall-project/src/test/java/com/example/gulimall/project/GulimallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gulimall.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
